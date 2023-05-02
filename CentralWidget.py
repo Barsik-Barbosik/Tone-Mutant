c504e8bcaf1d8f0d0557fa62842366b5df5d9e7d
BarsikBarbosik <Barsik@Barbosik>
2023-05-02 22:54:42 +0300

    dsp effects: right side for all DSPs

--- CentralWidget.py
+++ CentralWidget.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QTabWidget, QFormLayout, QDial
     QLabel, QListWidget, QHBoxLayout, QSpinBox, QSizePolicy, QComboBox, QListWidgetItem
 
 from enums.enums import ParameterType
-from model.DspEffect import DspEffect
 from model.DspParameter import DspParameter
 from model.MainModel import MainModel
 
@@ -15,24 +14,20 @@ class CentralWidget(QWidget):
         super().__init__(*args, **kwargs)
 
         self.main_model = MainModel()
+
         main_layout = QGridLayout(self)
         self.setLayout(main_layout)
 
-        self.qgrid_layout1 = QGridLayout(self)
-        self.qgrid_layout2 = QGridLayout(self)
-        self.qgrid_layout3 = QGridLayout(self)
-        self.qgrid_layout4 = QGridLayout(self)
-
         tab_widget = QTabWidget(self)
-        tab_widget.addTab(self.create_dsp_page(self.main_model.currentDsp1), 'DSP 1')
-        # tab_widget.addTab(self.create_dsp_page(self.main_model.currentDsp2), 'DSP 2')
-        # tab_widget.addTab(self.create_dsp_page(self.main_model.currentDsp3), 'DSP 3')
-        # tab_widget.addTab(self.create_dsp_page(self.main_model.currentDsp4), 'DSP 4')
+        tab_widget.addTab(self.create_dsp_page(QGridLayout(self)), 'DSP 1')
+        tab_widget.addTab(self.create_dsp_page(QGridLayout(self)), 'DSP 2')
+        tab_widget.addTab(self.create_dsp_page(QGridLayout(self)), 'DSP 3')
+        tab_widget.addTab(self.create_dsp_page(QGridLayout(self)), 'DSP 4')
         tab_widget.addTab(self.create_main_params_page(), 'Main parameters')  # TODO: should be the first tab
 
         main_layout.addWidget(tab_widget, 0, 0, 2, 1)
 
-    def create_dsp_page(self, current_dsp: DspEffect):
+    def create_dsp_page(self, qgrid_layout: QGridLayout):
         dsp_page = QWidget(self)
         hbox_layout = QHBoxLayout(self)
         dsp_page.setLayout(hbox_layout)
@@ -45,29 +40,29 @@ class CentralWidget(QWidget):
             item.setData(Qt.UserRole, dsp_effect.id)
             list_widget.insertItem(idx + 1, item)
 
-        list_widget.itemClicked.connect(lambda state, lw=list_widget: self.on_list_widget_click(lw))
+        list_widget.itemClicked.connect(lambda state, lw=list_widget: self.on_list_widget_click(lw, qgrid_layout))
         hbox_layout.addWidget(list_widget)  # left side
 
-        self.update_parameter_panel()
-        hbox_layout.addLayout(self.qgrid_layout1)  # right side
+        self.update_parameter_panel(qgrid_layout)
+        hbox_layout.addLayout(qgrid_layout)  # right side
 
         return dsp_page
 
-    def update_parameter_panel(self):
-        self.clear_layout(self.qgrid_layout1)
+    def update_parameter_panel(self, qgrid_layout: QGridLayout):
+        self.clear_layout(qgrid_layout)
 
         if self.main_model.currentDsp1 is not None:
             for idx, dsp_parameter in enumerate(self.main_model.currentDsp1.dsp_parameter_list):
                 if dsp_parameter.type == ParameterType.COMBO:
-                    self.create_combo_input(dsp_parameter, idx, self.qgrid_layout1)
+                    self.create_combo_input(dsp_parameter, idx, qgrid_layout)
                 elif dsp_parameter.type == ParameterType.KNOB:
-                    self.create_knob_input(dsp_parameter, idx, self.qgrid_layout1)
+                    self.create_knob_input(dsp_parameter, idx, qgrid_layout)
         else:
-            self.qgrid_layout1.addWidget(QLabel("------------------- OFF ----------------"), 0, 0)
+            qgrid_layout.addWidget(QLabel("------------------- OFF ----------------"), 0, 0)
 
         empty_filler = QWidget(self)
         empty_filler.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-        self.qgrid_layout1.addWidget(empty_filler)
+        qgrid_layout.addWidget(empty_filler)
 
     def create_combo_input(self, dsp_parameter: DspParameter, idx: int, qgrid_layout: QGridLayout):
         print("creating combo for " + dsp_parameter.name)
@@ -86,19 +81,21 @@ class CentralWidget(QWidget):
         knob.setMinimum(knob_value_input.minimum())
         knob.setMaximum(knob_value_input.maximum())
         knob.setFixedSize(KNOB_SIZE, KNOB_SIZE)
-        knob.valueChanged.connect(lambda state, kn=knob, inp=knob_value_input: self.on_knob_changed(kn, inp, dsp_parameter.name))
-        knob_value_input.valueChanged.connect(lambda state, inp=knob_value_input, kn=knob: self.on_knob_spinbox_changed(inp, kn, dsp_parameter.name))
+        knob.valueChanged.connect(
+            lambda state, kn=knob, inp=knob_value_input: self.on_knob_changed(kn, inp, dsp_parameter.name))
+        knob_value_input.valueChanged.connect(
+            lambda state, inp=knob_value_input, kn=knob: self.on_knob_spinbox_changed(inp, kn, dsp_parameter.name))
         hbox = QHBoxLayout(self)
         hbox.addWidget(knob_value_input)
         hbox.addWidget(knob)
         qgrid_layout.addWidget(QLabel(dsp_parameter.name + ":"), idx, 0)
         qgrid_layout.addLayout(hbox, idx, 1)
 
-    def on_list_widget_click(self, list_widget: QListWidget):
+    def on_list_widget_click(self, list_widget: QListWidget, qgrid_layout: QGridLayout):
         item_id: int = list_widget.currentItem().data(Qt.UserRole)
         print(str(item_id) + " - " + list_widget.currentItem().text())
         self.main_model.setCurrentDsp1(item_id)
-        self.update_parameter_panel()
+        self.update_parameter_panel(qgrid_layout)
 
     @staticmethod
     def on_combo_changed(combo: QComboBox, dsp_parameter_name: str):
