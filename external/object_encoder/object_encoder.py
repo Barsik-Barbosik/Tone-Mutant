7396bf0ece75daab87782f4769080ea9cf53ab84
BarsikBarbosik <Barsik@Barbosik>
2023-05-19 01:06:06 +0300

    json object encoder

---
+++ external/object_encoder/object_encoder.py
@@ -0,0 +1,26 @@
+import inspect
+import json
+
+
+# More general version of an Encoder for Python objects that can handle nested objects and inherited fields.
+# Author: tobigue; https://stackoverflow.com/questions/3768895/how-to-make-a-class-json-serializable
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "to_json"):
+            return self.default(obj.to_json())
+        elif hasattr(obj, "__dict__"):
+            d = dict(
+                (key, value)
+                for key, value in inspect.getmembers(obj)
+                if not key.startswith("__")
+                and not inspect.isabstract(value)
+                and not inspect.isbuiltin(value)
+                and not inspect.isfunction(value)
+                and not inspect.isgenerator(value)
+                and not inspect.isgeneratorfunction(value)
+                and not inspect.ismethod(value)
+                and not inspect.ismethoddescriptor(value)
+                and not inspect.isroutine(value)
+            )
+            return self.default(d)
+        return obj
