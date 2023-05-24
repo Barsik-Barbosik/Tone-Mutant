5a40c4023f5880825ae3f670dc539d758410d636
BarsikBarbosik <Barsik@Barbosik>
2023-05-24 14:12:35 +0300

    instrument params order

--- model/instrument.py
+++ model/instrument.py
@@ -1,9 +1,9 @@
 class Instrument:
-    def __init__(self, id: int, name: str, program_change: int, bank: int):
+    def __init__(self, id: int, name: str, bank: int, program_change: int):
         self.id = id
         self.name = name
-        self.program_change = program_change
         self.bank = bank
+        self.program_change = program_change
 
     def to_json(self):
         return {"id": self.id, "name": self.name}
