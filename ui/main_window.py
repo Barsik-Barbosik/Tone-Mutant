5a434774ba39cd4c45de151b57d945ecfd58c1fa
BarsikBarbosik <Barsik@Barbosik>
2025-01-27 21:45:00 +0200

    version 1.3.0

--- ui/main_window.py
+++ ui/main_window.py
@@ -28,7 +28,7 @@ from utils.utils import resource_path
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Tone Mutant 1.2.5")
+        self.setWindowTitle("Tone Mutant 1.3.0")
         self.setWindowIcon(QIcon(resource_path("resources/note.png")))
 
         self.status_bar = QStatusBar(self)
