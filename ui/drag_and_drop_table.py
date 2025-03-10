d64afbf859a091f4d31c3fcb3897f677e559448e
BarsikBarbosik <Barsik@Barbosik>
2025-03-11 01:18:13 +0200

    bug fix: unhandled win32 exception occured in [5040] python.exe

--- ui/drag_and_drop_table.py
+++ ui/drag_and_drop_table.py
@@ -1,4 +1,4 @@
-from PySide2.QtCore import Qt, QMimeData
+from PySide2.QtCore import Qt, QMimeData, QMetaObject
 from PySide2.QtGui import QDrag
 from PySide2.QtWidgets import QTableWidget, QTableWidgetItem, QAbstractItemView, QLineEdit, QItemDelegate
 
@@ -130,7 +130,8 @@ class DragAndDropTable(QTableWidget):
         self.setItem(row, 0, QTableWidgetItem(item_text))  # Add the item to the row
 
         # After adding a new item, update the row numbers starting from table_row_offset
-        self._update_row_numbers()
+        # Trying to prevent crash: call the method in the next event loop
+        QMetaObject.invokeMethod(self, "_update_row_numbers", Qt.QueuedConnection)
 
     def itemExists(self, text):
         """Checks if an item with the given text already exists in the table."""
